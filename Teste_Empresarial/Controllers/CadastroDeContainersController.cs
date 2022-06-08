606da642c8f742d2216af92c1cf89ba8cefd8d76
cadusouzar <cadusouzaribeiro@hotmail.com>
2022-06-08 04:20:28 -0300

    commit solution

---
+++ Teste_Empresarial/Controllers/CadastroDeContainersController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Teste_Empresarial.Models;
+
+namespace Teste_Empresarial.Controllers
+{
+    public class CadastroDeContainersController : Controller
+    {
+        private readonly Teste_EmpresarialContext _context;
+
+        public CadastroDeContainersController(Teste_EmpresarialContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CadastroDeContainers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CadastroDeContainers.ToListAsync());
+        }
+
+        // GET: CadastroDeContainers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cadastroDeContainer = await _context.CadastroDeContainers
+                .FirstOrDefaultAsync(m => m.ContainerID == id);
+            if (cadastroDeContainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(cadastroDeContainer);
+        }
+
+        // GET: CadastroDeContainers/Create
+        public IActionResult CadastroCreateOrEdit(int id = 0)
+        {
+            if(id == 0)
+            {
+                return View(new CadastroDeContainer());
+            }
+            else
+            {
+                return View(_context.CadastroDeContainers.Find(id));
+            }
+        }
+
+        // POST: CadastroDeContainers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CadastroCreateOrEdit([Bind("ContainerID,FullName,ContainerNumber,Type,Status,Category")] CadastroDeContainer cadastroDeContainer)
+        {
+            if (ModelState.IsValid)
+            {
+                if(cadastroDeContainer.ContainerID == 0)
+                {
+                    _context.Add(cadastroDeContainer);
+                }
+                else
+                {
+                    _context.Update(cadastroDeContainer);
+                }
+                _context.Add(cadastroDeContainer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(cadastroDeContainer);
+        }
+
+        // GET: CadastroDeContainers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var cadastroDeContainer = await _context.CadastroDeContainers.FindAsync(id);
+            _context.CadastroDeContainers.Remove(cadastroDeContainer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool CadastroDeContainerExists(int id)
+        {
+            return _context.CadastroDeContainers.Any(e => e.ContainerID == id);
+        }
+    }
+}
---
+++ Teste_Empresarial/Migrations/20220608030651_InicialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teste_Empresarial.Migrations
+{
+    public partial class InicialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CadastroDeContainers",
+                columns: table => new
+                {
+                    ContainerID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    ContainerNumber = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Category = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CadastroDeContainers", x => x.ContainerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovimentacaoDeContainers",
+                columns: table => new
+                {
+                    MovementID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContainerNumber = table.Column<string>(nullable: false),
+                    TypeOfMovement = table.Column<int>(nullable: false),
+                    InitialDate = table.Column<DateTime>(nullable: false),
+                    FinalDate = table.Column<DateTime>(nullable: false),
+                    CadastroDeContainerContainerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovimentacaoDeContainers", x => x.MovementID);
+                    table.ForeignKey(
+                        name: "FK_MovimentacaoDeContainers_CadastroDeContainers_CadastroDeContainerContainerID",
+                        column: x => x.CadastroDeContainerContainerID,
+                        principalTable: "CadastroDeContainers",
+                        principalColumn: "ContainerID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovimentacaoDeContainers_CadastroDeContainerContainerID",
+                table: "MovimentacaoDeContainers",
+                column: "CadastroDeContainerContainerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovimentacaoDeContainers");
+
+            migrationBuilder.DropTable(
+                name: "CadastroDeContainers");
+        }
+    }
+}
